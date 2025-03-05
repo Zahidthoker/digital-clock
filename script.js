bb744f9f8461fd1a6eb7c8006134b445fb83236e
zahid <zahidthoker5@gmail.com>
2025-03-05 10:32:24 +0530

    resolved the design isssue

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ const amPm = document.getElementById('ampm');
 function getTime(){
     const time = new Date();
     const hour = (time.getHours())%12;
-    const minutues = time.getMinutes();
+    const hours = hour<10?'0'+hour:hour;
+    const minutues = time.getMinutes()<10?'0'+time.getMinutes():time.getMinutes();
     const seconds = time.getSeconds();
+    second = seconds<10?'0'+seconds:seconds;
     const ampm = time.getHours()<=12?'AM':'PM';
-    timeElement.innerText = `${hour}:${minutues}:${seconds}:${ampm}`
+    timeElement.innerText = `${hours}:${minutues}:${second}:${ampm}`
 
     const year = time.getFullYear();
     const monthname = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     const month = time.getMonth();
-    const day = time.getDate();
+    const day = time.getDate()<10?'0'+time.getDate():time.getDate();
 
     dateElement.innerText = `${day}-${monthname[month+1]}-${year}`
 }
