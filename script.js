8044354b7bf172e9d309a4f576e17cf2d6033b4d
zahid <zahidthoker5@gmail.com>
2025-03-13 15:44:30 +0530

    modified

--- script.js
+++ script.js
@@ -6,18 +6,32 @@ function getTime(){
     const time = new Date();
     const hour = (time.getHours())%12||12;
     const hours = hour<10?'0'+hour:hour;
-    const minutues = time.getMinutes()<10?'0'+time.getMinutes():time.getMinutes();
+    // const minutes = time.getMinutes().toString().padStart(2,'0');
+    const minutes = time.getMinutes()<10?'0'+time.getMinutes():time.getMinutes();
     const seconds = time.getSeconds();
     second = seconds<10?'0'+seconds:seconds;
     const ampm = time.getHours()<=12?'AM':'PM';
-    timeElement.innerText = `${hours}:${minutues}:${second}:${ampm}`
+    timeElement.innerText = `${hours}:${minutes}:${second}:${ampm}`
 
-    const year = time.getFullYear();
-    const monthname = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    const month = time.getMonth();
-    const day = time.getDate()<10?'0'+time.getDate():time.getDate();
+    const options = {
+        weekday:"long", //we can set it as short
+        year:"numeric",
+        month:"long",
+        day:"numeric"
+    }
 
-    dateElement.innerText = `${day}-${monthname[month+1]}-${year}`
+    // basic code to set the date part without using the toLocalDatesString
+
+
+    // const year = time.getFullYear();
+    // const monthname = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    // const month = time.getMonth();
+    // const day = time.getDate()<10?'0'+time.getDate():time.getDate();
+    // dateElement.innerText = `${day}-${monthname[month+1]}-${year}`
+
+
+    const datestring = time.toLocaleDateString(undefined,options)
+    dateElement.innerText = `${datestring}`
 }
 
 setInterval(getTime,1000)
